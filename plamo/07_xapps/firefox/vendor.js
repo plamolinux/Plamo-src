d7d5be6fa31fbaebb81df4284d6bc950845a9653
KATOH Yasufumi <karma@jazz.email.ne.jp>
2018-01-30 17:58:18 +0900

    plamo/01_minimum/network.txz/ntp: こじまさんのスクリプトに処理を合わせた
    * /etc/rc.d/rc?.d からのリンク処理

---
+++ plamo/07_xapps/firefox/vendor.js
@@ -0,0 +1,9 @@
+// If this sets true, use LANG environment variable to choose locale
+pref("intl.locale.matchOS", false);
+
+// Use ja as locale
+pref("general.useragent.locale", "ja");
+
+// Disable default browser checking.
+pref("browser.shell.checkDefaultBrowser", false);
+
