73ed2a68aafac52072a27da4b30559a546bae8d7
KATOH Yasufumi <karma@jazz.email.ne.jp>
2018-01-30 21:34:45 +0900

    plamo/07_xapps/firefox: 58.0.1
    テストパッケージなので B0 としています
    
    * 日本語言語パックをアドオンとして入れてありますが有効化されていません
    * 初回起動時に有効化し、再起動すると日本語になります（Nighty→Mozilla Firefoxになる）

--- plamo/07_xapps/firefox/vendor.js
+++ plamo/07_xapps/firefox/vendor.js
@@ -1,8 +1,5 @@
 // If this sets true, use LANG environment variable to choose locale
-pref("intl.locale.matchOS", false);
-
-// Use ja as locale
-pref("general.useragent.locale", "ja");
+pref("intl.locale.matchOS", true);
 
 // Disable default browser checking.
 pref("browser.shell.checkDefaultBrowser", false);
