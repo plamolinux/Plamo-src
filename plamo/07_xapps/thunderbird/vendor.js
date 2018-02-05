14bd9476db21f51003562a1eb8c3666259f4e671
KATOH Yasufumi <karma@jazz.email.ne.jp>
2018-02-05 20:25:38 +0900

    plamo/10_xfce/orage: 4.12.1

---
+++ plamo/07_xapps/thunderbird/vendor.js
@@ -0,0 +1,5 @@
+// If this sets true, use LANG environment variable to choose locale
+pref("intl.locale.matchOS", true);
+
+// Disable default mailer checking.
+pref("mail.shell.checkDefaultMail", false);
